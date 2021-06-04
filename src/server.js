371b0c9cf36596bda90965d3eb078f92040c4185
Hemant-soni-vst-au4 <sonihemant4741@gmail.com>
2021-06-04 22:45:59 +0530

    mongodb connection setups

--- src/server.js
+++ src/server.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const mongoose = require('mongoose');
+
 const PORT = process.env.PORT || 8000;
+
+if(process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
 
 app.use(cors());
 app.use(express.json());
@@ -9,6 +15,16 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.send('Hello everyone');
 })
+
+try {
+   mongoose.connect(process.env.MONGO_DB_CONNECTION, {
+       useNewUrlParser: true,
+       useUnifiedTopology: true,
+   })
+   console.log('MONGODb Connected')
+} catch(error) {
+     console.log(error);
+}
 
 app.listen(PORT, () => {
     console.log(`listening on port ${PORT}`);
