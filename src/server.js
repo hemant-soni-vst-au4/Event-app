6f03eba297a110862342b34e8ec9150ee3c03dc5
Hemant-soni-vst-au4 <sonihemant4741@gmail.com>
2021-06-04 21:33:19 +0530

    middleware in server.js

--- src/server.js
+++ src/server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
-
+const cors = require('cors');
 const PORT = process.env.PORT || 8000;
+
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Hello everyone');
