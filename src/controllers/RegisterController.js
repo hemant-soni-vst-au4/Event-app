8459212406c1dfb18fd1a0c5d395403d3687080b
Hemant-soni-vst-au4 <sonihemant4741@gmail.com>
2021-06-06 22:35:40 +0530

    user controller sending data to mongoDB

--- src/controllers/RegisterController.js
+++ src/controllers/RegisterController.js
@@ -1,4 +1,30 @@
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
 
+module.exports = {
+  async store(req, res) {
+    try {
+      const { firstName, lastName, password, email } = req.body;
+      console.log(req.body);
+      const existenUser = await User.findOne({ email });
+      console.log(existenUser);
+      if (!existenUser) {
+        const hashPasword = await bcrypt.hash(password, 10);
+        const user = await User.create({
+          firstName,
+          lastName,
+          password: hashPasword,
+          email,
+        });
 
+        return res.json(user);
+      }
+
+      return res.status(400).json({
+        message: "email/user already exist",
+      });
+    } catch (error) {
+      throw Error(error);
+    }
+  },
+};
