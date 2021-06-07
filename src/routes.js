5d87d56b443bab96455f8a8d41be6fe740fd35ab
Hemant-soni-vst-au4 <sonihemant4741@gmail.com>
2021-06-07 22:51:49 +0530

    Event controller created

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,17 @@
 const express = require('express');
+const multer = require('multer');
 const routes = express.Router();
 const UserController = require('./controllers/UserController');
+const EventController = require('./controllers/EventController');
 
-
+const uploadConfig =require('./config/upload')
+const upload = multer(uploadConfig);
 
 routes.get('/status', (req, res) => {
     res.send({ status: 200 });
 })
 
-
+routes.post('/event', upload.single(thumbnail), EventController.createEvent)
 routes.post('/user/register', UserController.createUser);
 routes.get('/user/:userId', UserController.getUserById)
 
