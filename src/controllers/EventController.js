8e1475d2bd4b99203120333e7f3bd2c6157ab690
Hemant-soni-vst-au4 <sonihemant4741@gmail.com>
2021-06-07 22:36:20 +0530

    multer config

--- src/controllers/EventController.js
+++ src/controllers/EventController.js
@@ -1,8 +1,27 @@
-const Event = ('../models/Event.js')
+const Event = require('../models/Event.js');
+const User = require('../models/User.js');
 
 
 module.export = {
     async createEvent (req, res) {
-        const {title, description, price } = req.body; 
+        const {title, description, price } = req.body;        
+        const { userId } = req.headers;
+        const { filename } = req.file,
+        
+        const user = await User.findById(userId);
+
+        if(!user) {
+            return res.status(404).json({ message: 'user is not exist' });
+        }
+
+        const event = Event.create({
+            title,
+            description,
+            price,
+            user: userId,
+            thumbnail: filename,
+        })
+
+        return res.json(event);
     }
 }
