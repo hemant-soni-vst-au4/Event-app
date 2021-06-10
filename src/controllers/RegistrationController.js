14a7b87dbb612a74da8cdbd617e0f247a8ba7225
Hemant-soni-vst-au4 <sonihemant4741@gmail.com>
2021-06-11 00:43:27 +0530

    approval controller

--- src/controllers/RegistrationController.js
+++ src/controllers/RegistrationController.js
@@ -1,33 +1,38 @@
 const Registration = require("../models/Registration.js");
 
 module.exports = {
-    async create(req, res) {
-       const { user_id } = req.headers;
-       const { eventId } = req.params;
-       const { date } = req.body;
+  async create(req, res) {
+    const { user_id } = req.headers;
+    const { eventId } = req.params;
+    const { date } = req.body;
 
-       const registration = Registration.create({
-           user: user_id,
-           event: eventId,
-           date
-       })
-       await registration
-        .populate('event')
-        .populate('user', '-password')
+    const registration = Registration.create({
+      user: user_id,
+      event: eventId,
+      date,
+    });
+    await registration
+      .populate("event")
+      .populate("user", "-password")
+      .execPopulate();
+
+    return res.json(registration);
+  },
+
+  async getRegistration(req, res) {
+    const { registration_id } = req.params;
+
+    try {
+      const registration = await Registration.findById(registration_id);
+
+      await registration
+        .populate("event")
+        .populate("user", "-password")
         .execPopulate();
-        
-       return res.json(registration);
-    },
 
-    async getRegistration(req, res) {
-        const {registration_id } = req.params;
-
-        try {
-            const registration = await Registration.findById(registration_id);
-
-            return res.json(registration);
-        } catch (error) {
-            return res.status(400).json({message: 'Registration not found'});
-        }
+      return res.json(registration);
+    } catch (error) {
+      return res.status(400).json({ message: "Registration not found" });
     }
-}
+  },
+};
