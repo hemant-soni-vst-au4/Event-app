802ee3580afbd79106339356f56e691b64faaa08
Hemant-soni-vst-au4 <sonihemant4741@gmail.com>
2021-06-13 11:45:41 +0530

    dashboard component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
-import Login from './pages/Login'
+import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
+
 function App() {
   return (
     <div>
     <h1>Hello Hemant</h1>
     <Login />
+    <Dashboard />
     </div>
   );
 }
