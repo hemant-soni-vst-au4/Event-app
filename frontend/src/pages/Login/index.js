2631973eed2151ead47264f9c7e2351d760b398c
Hemant-soni-vst-au4 <sonihemant4741@gmail.com>
2021-06-14 21:20:06 +0530

    login form

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React from "react";
+import api from "../../services/api";
 
-export default function Login(){
-    return(
-        <div>
-            Hello free login
-        </div>
-    )
+export default function Login() {
+  return (
+    <Form>
+      <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
+        <Label for="exampleEmail" className="mr-sm-2">
+          Email
+        </Label>
+        <Input
+          type="email"
+          name="email"
+          id="exampleEmail"
+          placeholder="something@idk.cool"
+        />
+      </FormGroup>
+      <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
+        <Label for="examplePassword" className="mr-sm-2">
+          Password
+        </Label>
+        <Input
+          type="password"
+          name="password"
+          id="examplePassword"
+          placeholder="don't tell!"
+        />
+      </FormGroup>
+      <Button>Submit</Button>
+    </Form>
+  );
 }
