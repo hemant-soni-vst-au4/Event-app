d05ce75cb435f427367121cb00a35bc6b35cc1be
Hemant-soni-vst-au4 <sonihemant4741@gmail.com>
2021-06-15 23:19:54 +0530

    register connection with api

--- frontend/src/Register/index.js
+++ frontend/src/Register/index.js
@@ -1,65 +1,76 @@
 import React, { useState } from "react";
 import api from "../../services/api";
-import { Button, Form, FormGroup, Input } from 'reactstrap';
+import { Button, Form, FormGroup, Input, Container } from "reactstrap";
 
-export default function Register({history}) {
-   const [ email, setEmail ] = useState("");
-   const [ password, setPassword ] = useState("");
-   const [ firstName, setFirstName ] = useState("");
-   const [ lastName, setLastName] = useState("");
+export default function Register({ history }) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
 
-   const handleSubmit = async event =>{
-       event.preventDefault();
-       console.log("changes are", email, password);
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    console.log("changes are", email, password);
 
-      const respone = await api.post('/login', {email, password, firstName, lastName});
+    const respone = await api.post("/user/register", {
+      email,
+      password,
+      firstName,
+      lastName,
+    });
 
-      const userId = respone.data._id || false;
-      if(userId) {
-          localStorage.setItem('user', userId);
-          history.push('/dashboard')
-      } else {
-          const {message} = respone.data;
-          console.log(message)
-      }
+    const userId = respone.data._id || false;
+    if (userId) {
+      localStorage.setItem("user", userId);
+      history.push("/dashboard");
+    } else {
+      const { message } = respone.data;
+      console.log(message);
     }
+  };
 
   return (
-    <Form onsubmit={handleSubmit}>
+    <Container>
+      <h2>Please Register your account</h2>
+      <Form onsubmit={handleSubmit}>
         <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
-        <Input
-          type="text"
-          name="firstname"
-          id="firstname"
-          placeholder="Your first name" onChange={evt => setFirstName(evt.target.value)}
-        />
-      </FormGroup>
-      <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
-        <Input
-          type="text"
-          name="lastName"
-          id="lastName"
-          placeholder="Your last Name" onChange={evt => setLastName(evt.target.value)}
-        />
-      </FormGroup>
-      <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
-        <Input
-          type="email"
-          name="email"
-          id="email"
-          placeholder="Your email address" onChange={evt => setEmail(evt.target.value)}
-        />
-      </FormGroup>
-      <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
-        
-        <Input
-          type="password"
-          name="password"
-          id="password"
-          placeholder="Enter the Password" onChange={evt => setPassword(evt.target.value)}
-        />
-      </FormGroup>
-      <Button onClick={(e) => handleSubmit(e)}>Submit</Button>
-    </Form>
+          <Input
+            type="text"
+            name="firstname"
+            id="firstname"
+            placeholder="Your first name"
+            onChange={(evt) => setFirstName(evt.target.value)}
+          />
+        </FormGroup>
+        <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
+          <Input
+            type="text"
+            name="lastName"
+            id="lastName"
+            placeholder="Your last Name"
+            onChange={(evt) => setLastName(evt.target.value)}
+          />
+        </FormGroup>
+        <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
+          <Input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Your email address"
+            onChange={(evt) => setEmail(evt.target.value)}
+          />
+        </FormGroup>
+        <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
+          <Input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Enter the Password"
+            onChange={(evt) => setPassword(evt.target.value)}
+          />
+        </FormGroup>
+        <Button onClick={(e) => handleSubmit(e)}>Submit</Button>
+      </Form>
+    </Container>
   );
 }
