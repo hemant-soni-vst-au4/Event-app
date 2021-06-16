8016e7ad3bc8758203d617bdfef24ea18171361d
Hemant-soni-vst-au4 <sonihemant4741@gmail.com>
2021-06-16 23:47:35 +0530

    Event page and route

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -3,6 +3,7 @@ import { BrowserRouter , Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
 import Register from './pages/Register';
+import Event from './pages/Event';
 
 export default function Routes() {
     return (
@@ -11,6 +12,7 @@ export default function Routes() {
           <Route path="/" exact component={Login} />
           <Route path="/dashboard" component={Dashboard} />
           <Route path="/register" exact component={Register} />
+          <Route path="/events" exact component={Event} />
         </Switch>
         </BrowserRouter>
     )
