a9570c09f1b8fb91a906055e74a73da1d9f7fb33
Hemant-soni-vst-au4 <sonihemant4741@gmail.com>
2021-06-19 23:01:21 +0530

    state setup in event module

--- frontend/src/pages/Event/index.js
+++ frontend/src/pages/Event/index.js
@@ -1,8 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Event() {
-    const user_id = localStorage.getItem('user')
-    console.log(user_id)
+    const user_id = localStorage.getItem('user');
+
+    const [title, setTitle] = useState('');
+    const [ description, setDescription] = useState('');
+    const [price, setPrice] = useState('');
+    const [thumbnail, setThumbnail] = useState('');
+    const [date, setDate] = useState('');
+
     return (
         <div>
             Hello from Event
