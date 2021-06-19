5a09dd6dbb59e14ddbe213afa4871e6e79fcf8f0
Hemant-soni-vst-au4 <sonihemant4741@gmail.com>
2021-06-19 23:13:35 +0530

    layout for event form

--- frontend/src/pages/Event/index.js
+++ frontend/src/pages/Event/index.js
@@ -1,18 +1,29 @@
 import React, { useState } from 'react'
+import api from '../../services/api';
+import { Button, Form, FormGroup, Input, label, Container } from "reactstrap";
 
 export default function Event() {
     const user_id = localStorage.getItem('user');
 
     const [title, setTitle] = useState('');
-    const [ description, setDescription] = useState('');
+    const [description, setDescription] = useState('');
     const [price, setPrice] = useState('');
     const [thumbnail, setThumbnail] = useState('');
     const [date, setDate] = useState('');
 
+    const handleSubmit = () => {
+
+    }
+
     return (
-        <div>
-            Hello from Event
-        </div>
+        <Container>
+            <h1>Create your Event</h1>
+            <Form onSubmit={handleSubmit}>
+                <label>upload Image</label>
+                <input id="thumbnail" type="file" onChange={e => setThumbnail(e.target.files(0))}/>
+               
+            </Form>
+        </Container>
     )
 }
 
