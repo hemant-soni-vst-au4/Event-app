e7f77c803a5ec67b5c03ad2b76d060d76a6c0a19
Hemant-soni-vst-au4 <sonihemant4741@gmail.com>
2021-06-22 22:18:19 +0530

    getting event data in dashboard component

--- frontend/src/pages/Dashboard/index.js
+++ frontend/src/pages/Dashboard/index.js
@@ -5,7 +5,13 @@ export default function Dashboard(){
     const [events, setEvents] = useState([]);
     const user_id = localStorage.getItem('user');
 
-    
+    const getEvents = async (filter) => {
+
+        const url = filter ? '/dashboard/${filter}' : '/dashboard'
+        const response = await api.get(url, {headers: { user_id}});
+
+        setEvents(response.data);
+    }
     return(
         <div>
             Hello from Dashboard
