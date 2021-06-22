8555dadfe148a3df31de11ad51d138ee604054b4
Hemant-soni-vst-au4 <sonihemant4741@gmail.com>
2021-06-22 21:58:38 +0530

    update in dashboard component

--- frontend/src/pages/Dashboard/index.js
+++ frontend/src/pages/Dashboard/index.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react';
+import api from "../../../services/api";
 
 export default function Dashboard(){
+    const [events, setEvents] = useState([]);
     const user_id = localStorage.getItem('user');
 
-    console.log(user_id)
+    
     return(
         <div>
             Hello from Dashboard
