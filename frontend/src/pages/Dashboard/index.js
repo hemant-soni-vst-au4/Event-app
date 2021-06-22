351c3c7f29b78bd745b921f9c456905b6cc97d7a
Hemant-soni-vst-au4 <sonihemant4741@gmail.com>
2021-06-22 22:46:17 +0530

    event data render in dashboard component

--- frontend/src/pages/Dashboard/index.js
+++ frontend/src/pages/Dashboard/index.js
@@ -1,9 +1,15 @@
 import React, {useEffect, useState} from 'react';
 import api from "../../../services/api";
+import moment from "moment";
+import './dshboard.css';
 
 export default function Dashboard(){
     const [events, setEvents] = useState([]);
     const user_id = localStorage.getItem('user');
+
+    useEffect(() => {
+      getEvents()
+    }, [])
 
     const getEvents = async (filter) => {
 
@@ -13,8 +19,19 @@ export default function Dashboard(){
         setEvents(response.data);
     }
     return(
-        <div>
-            Hello from Dashboard
-        </div>
+        <ul className="events-list">
+
+            {events.map(event =>(
+                <li key={event._id}>
+                  <header style={{ backgroundImage: `url(${event.thumbnail_url})` }} />
+                  <strong>{event.title}</strong>
+                  <span>{moment(event.date).format('DDMMYY')}</span>
+                  <span>{event.price}</span>
+                  <span>{event.description}</span>
+                  
+                </li>
+            ))}
+
+        </ul>
     )
 }
