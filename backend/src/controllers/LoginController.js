e496843eaa89f848cd1ed0cf5e0afad1f02d2607
Hemant-soni-vst-au4 <sonihemant4741@gmail.com>
2021-06-25 13:31:55 +0530

    jwt use in login

--- backend/src/controllers/LoginController.js
+++ backend/src/controllers/LoginController.js
@@ -1,5 +1,6 @@
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken")
 
 module.exports = {
     async store(req, res) {
@@ -19,7 +20,13 @@ module.exports = {
                     firstName: user.firstName,
                     lastName: user.lastName
                 }
-                return res.json(userResponse);
+                return jwt.sign({user : userResponse}, 'secret', (err, token) => {
+                    return res.json({
+                        user: token,
+                        user_id: userResponse._id
+                    })
+                })
+                //return res.json(userResponse);
             } else {
                 return res.status(400).json({message: "Email and password does not match"});
             }
