232e23970fb2c325d8e81e7bef41bf13fb2e4414
Hemant-soni-vst-au4 <sonihemant4741@gmail.com>
2021-06-25 11:06:30 +0530

    dashboard style update

--- backend/src/controllers/DashboardController.js
+++ backend/src/controllers/DashboardController.js
@@ -40,4 +40,18 @@ module.exports = {
           return res.status(404).json({ message: "We don't have any Event yet" });
         }
       },
+      
+      async getEventsByUserId(req, res) {
+        const { user_id } = req.headers;
+
+        try {
+            const events = await Event.find({ user: user_id })
+
+            if (events) {
+                return res.json(events)
+            }
+        } catch (error) {
+            return res.status(400).json({ message: `We do have any events with the user_id ${user_id}`})
+        }
+    },
 }
